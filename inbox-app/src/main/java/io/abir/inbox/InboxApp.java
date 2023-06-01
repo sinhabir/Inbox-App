4602c2901a2a3790ddda6ce67082cf01376e9dff
Abir Sinha <Abir Sinha@LAPTOP-8SUK3I5A>
2023-06-01 18:13:38 +0200

    2nd Commit with Cassandra Config in place

--- inbox-app/src/main/java/io/abir/inbox/InboxApp.java
+++ inbox-app/src/main/java/io/abir/inbox/InboxApp.java
@@ -1,10 +1,11 @@
 package io.abir.inbox;
 
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,12 +15,13 @@ public class InboxApp {
 	public static void main(String[] args) {
 		SpringApplication.run(InboxApp.class, args);
 	}
-
-	@RequestMapping("/user")
-	public String user(@AuthenticationPrincipal OAuth2User principal) {
-		System.out.println(principal);
-		return principal.getAttribute("name");
-	}
-
-
+	
+ @Bean
+ public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraproperties) {
+	 Path bundle = astraproperties.getSecureConnectBundle().toPath();
+	 return builder -> builder.withCloudSecureConnectBundle(bundle);
+	 
+ }
+	
+	
 }
